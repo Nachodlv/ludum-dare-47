6fbae405301619157c971eb84a6a0978f6423ad3
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2020-10-02 23:46:40 -0300

    Add Player Test Scene

--- Assets/Scripts/AI/EnemyAI.cs
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using Entities.Enemy.Ai.States;
 using UnityEngine;
 
 namespace Entities.Enemy.Ai
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class EnemyAI : MonoBehaviour
     {
         [SerializeField] private Stats stats;
@@ -12,7 +14,18 @@ namespace Entities.Enemy.Ai
         private void Awake()
         {
             _stateMachine = new StateMachine();
+            var moveState = new MoveState(GetComponent<Rigidbody2D>(), stats);
+            _stateMachine.SetState(moveState);
+        }
 
+        private void Update()
+        {
+            _stateMachine.Tick();
+        }
+
+        private void FixedUpdate()
+        {
+            _stateMachine.FixedTick();
         }
     }
 }
