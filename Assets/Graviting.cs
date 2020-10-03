551bd26f9a8b5b53e59ef792c2355cf9bfd3b869
Dwape <eduardo.lalor@ing.austral.edu.ar>
2020-10-02 23:07:05 -0300

    Add DwapeScene

---
+++ Assets/Graviting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graviting : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    private float _gravityConstant = 1f; // 9.8f
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Assuming that the center of gravity is placed in (0, 0, 0)
+        Vector3 vector = gameObject.transform.position.normalized;
+        _rigidbody2D.AddForce(vector * -_gravityConstant);
+    }
+}
