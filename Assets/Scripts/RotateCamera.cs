07f2518f55ecfed3d8038f64cf69aa554b0e328c
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2020-10-03 17:51:48 -0300

    Rotate camera with player

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class RotateCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+        private void Update()
+        {
+            var playerPosition = player.position;
+            var angles = Vector2.SignedAngle(Vector2.down, playerPosition);
+            virtualCamera.m_Lens.Dutch = angles;
+        }
+    }
+}
