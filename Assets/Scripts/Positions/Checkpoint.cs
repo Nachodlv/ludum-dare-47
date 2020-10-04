000bb116088e52baef461d100d2756a467fdc219
Dwape <eduardo.lalor@ing.austral.edu.ar>
2020-10-03 22:50:02 -0300

    Add Checkpoint and CheckpointManager

---
+++ Assets/Scripts/Positions/Checkpoint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class Checkpoint : MonoBehaviour
+{
+
+    [SerializeField] private int checkpointLength = 20;
+    private float _angle;
+
+    private EdgeCollider2D _edgeCollider2D;
+    private  Vector2 _vector = Vector2.down; // How should this vector be determined?
+    // We should probably have someone that creates the checkpoints and defines a vector for each one.
+    
+    private int _angleThreshold = 300;
+
+    public delegate void CheckpointCallback(Racer racer, Checkpoint checkpoint);
+
+    public event CheckpointCallback OnRacerCheckpointReached;
+    
+    void Awake()
+    {
+        _edgeCollider2D = gameObject.GetComponent<EdgeCollider2D>();
+        _angle = CalculateAngle(_vector);
+    }
+
+    public void AssignAngle(float angle)
+    {
+        _angle = angle;
+        _vector = VectorFromAngle(_angle);
+        Vector2[] vectors = {Vector2.zero, _vector * checkpointLength};
+        _edgeCollider2D.points = vectors;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Racer racer = other.GetComponent<Racer>();
+        if (racer != null)
+        {
+            float racerAngle = CalculateAngle(racer.transform.position);
+            if (racerAngle - _angle > _angleThreshold || _angle > racerAngle)
+            {
+                OnRacerCheckpointReached?.Invoke(racer, this); // Is this ok?
+            }
+        }
+    }
+    
+    /**
+     * Calculates the Angle between the vector provided and a Vector2 pointing down.
+     */
+    private float CalculateAngle(Vector2 angle)
+    {
+        float value = Vector2.SignedAngle(Vector2.down, angle);
+        return value < 0 ? 360 + value : value;
+    }
+    
+    private Vector2 VectorFromAngle(float angle)
+    {
+        return Quaternion.Euler(0,0,angle) * Vector2.down;
+    }
+}
---
+++ Assets/Scripts/Positions/CheckpointManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+
+    [SerializeField] private Checkpoint checkpointPrefab;
+    [SerializeField] private int numberOfCheckpoints;
+
+    // Create a dictionary for storing the checkpoints of each Racer.
+    private Dictionary<Racer, Checkpoint> _checkpoints;
+
+    void Awake()
+    {
+        float increment = 360f / numberOfCheckpoints;
+        
+        for (int i = 0; i < numberOfCheckpoints; i++)
+        {
+            Checkpoint checkpoint = Instantiate(checkpointPrefab, Vector2.zero, Quaternion.identity);
+            checkpoint.AssignAngle(increment * i);
+            checkpoint.OnRacerCheckpointReached += RegisterCheckpoint;
+        }
+    }
+
+    public Checkpoint GetCheckpoint(Racer racer)
+    {
+        return _checkpoints[racer];
+    }
+
+    private void RegisterCheckpoint(Racer racer, Checkpoint checkpoint)
+    {
+        _checkpoints[racer] = checkpoint;
+        Debug.Log("Reached checkpoint!");
+    }
+}
