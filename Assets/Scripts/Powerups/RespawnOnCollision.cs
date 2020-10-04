2a42c08806b750c87f072fc7caaad844ac9fed57
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2020-10-04 19:35:53 -0300

    Implement spikes

---
+++ Assets/Scripts/Powerups/RespawnOnCollision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+
+namespace DefaultNamespace.Powerups
+{
+    public class RespawnOnCollision : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            var racer = other.gameObject.GetComponent<Racer>();
+            if (racer.IsPlayer)
+            {
+                racer.SpawnInLastCheckpoint();
+            }
+        }
+    }
+}
